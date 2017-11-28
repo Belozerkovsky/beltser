8d1a871dc291b14d1bd6fca30079cc88030142e2
Anastasia <lme96@mail.ru>
2017-11-28 12:29:26 +1000

    Lab_1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World :)');
 });
 
 app.listen(3005, function () {
